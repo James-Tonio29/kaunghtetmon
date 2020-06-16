1671cf5ba5f30c66a8770ccf4c88689b3814962a
James-Tonio29 <51860495+James-Tonio29@users.noreply.github.com>
2020-06-16 13:57:57 +0630

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+// const slide = document.querySelector('.vds');
+// const vids= document.querySelectorAll('.vds video');
+
+// const prevBtn=document.querySelector('#PrevBtn');
+// const nextBtn=document.querySelector('#NexBtn');
+
+// let count=1;
+// const size=vids[0].clientWidth;
+
+// slide.style.transform ='translateX('+(-size*count)+'px)';
+// NextBtn.addEventListener('click',function(){
+//     slide.style.transition = "transform 0.4s ease-in-out";
+//     count++;
+//     console.log(count);
+// });
+window.alert('This site is under construction');
